9074274f39c0deaa912d754ddea5e5512b3f8067
Sadia Afridi <sadiaafridi663@gmail.com>
2024-09-06 19:13:12 +0500

    add quiz game data

---
+++ script.js
@@ -0,0 +1,22 @@
+const quizData = [
+    {
+        question: "Which HTML tag is used to define an internal style sheet?",
+        options: ["<style>", "<css>", "link"],
+        correctAnswer: 2
+    },
+    {
+        question: "Which HTML tag is used to create a hyperlink?",
+        options: ["<a>", "<hyperlink>", "<href>"],
+        correctAnswer: 0
+    },
+    {
+        question: "Which JavaScript method is used to select an element by its ID",
+        options: ["getElementByClassName()", "getElementsByTagName()", "getElementById()"],
+        correctAnswer: 2
+    },
+    {
+        question: "What is the default display value for a <div> element in CSS?",
+        options: [" inline", "block", "inline-block"],
+        correctAnswer: 1
+    },
+];
